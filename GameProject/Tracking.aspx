c66730b5b2bbe437f78c206441e6bb1b54df9d4a
Robert <rob_reynolds_10@hotmail.com>
2016-06-23 12:18:07 -0400

    Added pagination to Tracking page

--- GameProject/Tracking.aspx
+++ GameProject/Tracking.aspx
@@ -17,7 +17,8 @@ File Description: tracking page --%>
                 <h1>Games List</h1>
                 <a href="Entry.aspx" class="btn btn-success btn-sm"><i class="fa fa-plus"></i> Add Game</a>
                 <asp:GridView runat="server" CssClass="table table-bordered table-striped table-hover" ID="GamesGridView" 
-                    AutoGenerateColumns="false" DataKeyNames="GameTitle" OnRowDeleting="GamesGridView_RowDeleting" >
+                    AutoGenerateColumns="false" DataKeyNames="GameTitle" OnRowDeleting="GamesGridView_RowDeleting" AllowPaging="true" 
+                    PageSize="3" OnPageIndexChanging="GamesGridView_PageIndexChanging" >
 
                     <Columns>
                         <%--<asp:BoundField DataField="GameID" HeaderText="Game ID" Visible="true" />--%>
--- GameProject/Tracking.aspx.cs
+++ GameProject/Tracking.aspx.cs
@@ -47,22 +47,65 @@ namespace GameProject
             }
         }
 
+        /**
+         * 
+         * <summary>
+         * This event handler deletes a game from the database using Entity Framework
+         * </summary>
+         * 
+         * @method GamesGridView_RowDeleting
+         * @param {object} sender
+         * @param {GridViewDeleteEventArgs} e
+         * @returns {void}
+         * 
+         */
         protected void GamesGridView_RowDeleting(object sender, GridViewDeleteEventArgs e)
         {
             //Store which row was clicked
             int selectedRow = e.RowIndex;
 
             //Get the selected GameID using the Grid's DataKey collection
-            int GameID = Convert.ToInt32(GamesGridView.DataKeys[selectedRow].Values["GameID"]);
+            //int GameID = Convert.ToInt32(GamesGridView.DataKeys[selectedRow].Values["GameID"]);
+            string GameTitle = Convert.ToString(GamesGridView.DataKeys[selectedRow].Values["GameTitle"]);
 
             //Use the Entity Framework to find the selected game in the database and remove it
             using (DefaultConnection db = new DefaultConnection())
             {
                 //Create object of game class and store the query string inside of it
-                //GameInfo deletedGame = (from GameInfo in db.GameInfoes
-                                        //where GameInfo.GameID == GameID;
-                                        //select GameInfo).FirstorDefault();
+                GameInfo deletedGame = (from GameInfo in db.GameInfoes
+                                        where GameInfo.GameTitle == GameTitle
+                                        select GameInfo).FirstOrDefault();
+
+                //Remove the selected game from the database
+                db.GameInfoes.Remove(deletedGame);
+
+                //Save changes to the database
+                db.SaveChanges();
+
+                //Refresh the GridView
+                this.GetGames();
             }
+        }
+
+        /**
+         * 
+         * <summary>
+         * This event handler allows pagination to occur for the game Tracking page
+         * </summary>
+         * 
+         * @method GamesGridView_PageIndexChanging
+         * @param {object} sender
+         * @param {GridViewPageEventArgs} e
+         * @returns {void}
+         * 
+         */
+        protected void GamesGridView_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            //Set the new page number
+            GamesGridView.PageIndex = e.NewPageIndex;
+
+            //Refresh the GridView
+            this.GetGames();
         }
     }
 }
